7c8aae63cb25b8b999bd66804d847377a81d638e
m.backi <108221259+cragHerk@users.noreply.github.com>
2023-10-09 23:56:14 +0200

    6

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -3,11 +3,35 @@ const AboutUs = () => {
   return (
     <div className={styles.aboutus}>
       <h2>O nas :</h2>
-      <span className={styles.aboutus_span}>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-        laboriosam ratione ipsam necessitatibus recusandae architecto iste quasi
-      </span>
-      <div className={styles.aboutus_wrapper}></div>
+      <div className={styles.aboutus_container}>
+        <div className={styles.aboutus_wrapper}>
+          <span className={styles.aboutus_span}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            laboriosam ratione ipsam necessitatibus recusandae architecto iste
+            quasi
+          </span>
+        </div>
+        <div className={styles.aboutus_wrapper2}>
+          <span className={styles.aboutus_span2}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            laboriosam ratione ipsam necessitatibus recusandae architecto iste
+            quasi
+          </span>
+        </div>
+        <div className={styles.aboutus_wrapper3}>
+          <span className={styles.aboutus_span3}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            laboriosam ratione ipsam necessitatibus recusandae architecto iste
+            quasi
+          </span>
+        </div>
+        <div className={styles.aboutus_wrapper4}>
+          <span className={styles.aboutus_span4}>
+            consectetur adipisicing elit. Quisquam, laboriosam ratione
+            recusandae architecto iste quasi
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
