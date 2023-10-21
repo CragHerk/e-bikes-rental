fa71cdeb0d324cc2628bec24fca9efd5b9f7803b
m.backi <108221259+cragHerk@users.noreply.github.com>
2023-10-21 12:30:05 +0200

    13

--- src/Components/Complete/Complete.jsx
+++ src/Components/Complete/Complete.jsx
@@ -1,5 +1,35 @@
 import styles from "./Complete.module.css";
+import { Link } from "react-router-dom";
+import MovingComponent from "react-moving-text";
 const Complete = () => {
-  return <div className={styles.complete}> your reservation has completed</div>;
+  return (
+    <div className={styles.complete}>
+      <Link to={"/"}>
+        <MovingComponent
+          type="fadeInFromTop"
+          duration="2500ms"
+          delay="0s"
+          direction="normal"
+          timing="linear"
+          iteration="1"
+          fillMode="none"
+        >
+          <button className={styles.btn}>Przejdź do strony głównej</button>
+        </MovingComponent>
+      </Link>
+      <MovingComponent
+        className={styles.wrapper}
+        type="popIn"
+        duration="1000ms"
+        delay="0s"
+        direction="normal"
+        timing="ease-in-out"
+        iteration="1"
+        fillMode="none"
+      >
+        <h1 className={styles.tag}>Zarezerwowano</h1>
+      </MovingComponent>
+    </div>
+  );
 };
 export default Complete;
