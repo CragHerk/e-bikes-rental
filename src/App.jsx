43f33fbf3282a997243dfc8ac9077df5862d0f87
m.backi <108221259+cragHerk@users.noreply.github.com>
2023-10-26 21:02:12 +0200

    drobiazg

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function App() {
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/complete" element={<Complete />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </Router>
   );
