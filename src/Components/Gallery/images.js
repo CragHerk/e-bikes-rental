513537a9b3bdd516541ff434f7cbc58eefe78192
m.backi <108221259+cragHerk@users.noreply.github.com>
2024-03-27 00:50:32 +0100

    web

--- src/Components/Gallery/images.js
+++ src/Components/Gallery/images.js
@@ -1,61 +1,61 @@
 export const images = [
   {
-    src: "assets/gallery/image12.jpg",
+    src: "assets/gallery/image12.webp",
     alt: "bike trip",
     thumbnail: "assets/thumbnails/thumbnail12.webp",
   },
   {
-    src: "/assets/gallery/image2.jpg",
+    src: "/assets/gallery/image2.webp",
     alt: "bike trip",
     thumbnail: "assets/thumbnails/thumbnail2.webp",
   },
   {
-    src: "/assets/gallery/image3.jpg",
+    src: "/assets/gallery/image3.webp",
     alt: "bike trip",
     thumbnail: "assets/thumbnails/thumbnail3.webp",
   },
   {
-    src: "/assets/gallery/image4.jpg",
+    src: "/assets/gallery/image4.webp",
     alt: "bike trip",
     thumbnail: "assets/thumbnails/thumbnail4.webp",
   },
   {
-    src: "/assets/gallery/image10.jpg",
+    src: "/assets/gallery/image10.webp",
     alt: "bike trip",
     thumbnail: "assets/thumbnails/thumbnail10.webp",
   },
   {
-    src: "/assets/gallery/image6.jpg",
+    src: "/assets/gallery/image6.webp",
     alt: "bike trip",
     thumbnail: "assets/thumbnails/thumbnail6.webp",
   },
   {
-    src: "/assets/gallery/image7.jpg",
+    src: "/assets/gallery/image7.webp",
     alt: "bike trip",
     thumbnail: "assets/thumbnails/thumbnail7.webp",
   },
   {
-    src: "/assets/gallery/image8.jpg",
+    src: "/assets/gallery/image8.webp",
     alt: "bike trip",
     thumbnail: "assets/thumbnails/thumbnail8.webp",
   },
   {
-    src: "/assets/gallery/image9.jpg",
+    src: "/assets/gallery/image9.webp",
     alt: "bike trip",
     thumbnail: "assets/thumbnails/thumbnail9.webp",
   },
   {
-    src: "/assets/gallery/image5.jpg",
+    src: "/assets/gallery/image5.webp",
     alt: "bike trip",
     thumbnail: "assets/thumbnails/thumbnail5.webp",
   },
   {
-    src: "/assets/gallery/image11.jpg",
+    src: "/assets/gallery/image11.webp",
     alt: "bike trip",
     thumbnail: "assets/thumbnails/thumbnail11.webp",
   },
   {
-    src: "/assets/gallery/image1.jpg",
+    src: "/assets/gallery/image1.webp",
     alt: "bike trip",
     thumbnail: "assets/thumbnails/thumbnail1.webp",
   },
