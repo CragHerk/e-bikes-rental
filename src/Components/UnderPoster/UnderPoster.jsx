c64418cbbd48a4cbd1e9710686ae016f0d2da184
m.backi <108221259+cragHerk@users.noreply.github.com>
2024-05-05 13:15:41 +0200

    1

--- src/Components/UnderPoster/UnderPoster.jsx
+++ src/Components/UnderPoster/UnderPoster.jsx
@@ -1,10 +1,20 @@
 import styles from "./UnderPoster.module.css";
+import { Link } from "react-router-dom";
 
 const UnderPoster = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.first}>ROWERY ELEKTRYCZNE</div>
-      <div className={styles.second}>SERWIS</div>
+      <div className={styles.second}>
+        {" "}
+        <Link
+          to={"/service"}
+          aria-label="link to service"
+          className={styles.link}
+        >
+          SERWIS
+        </Link>
+      </div>
       <div className={styles.third}>WYCIECZKI</div>
     </div>
   );
